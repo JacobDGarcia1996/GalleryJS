0d0f107a952c84f10bf92db07ae0c12b0d6c6f25
Jacob Garcia <jgarcia224@mail.csuchico.edu>
2017-04-25 13:30:34 -0700

    no longer repeats the same number

--- gallery.js
+++ gallery.js
@@ -1,6 +1,21 @@
-
+var Chico = [
+  "bidwell park",
+   "Chico State Univerity",
+   "Madison Bear Garden",
+    "Morning Thunder Cafe",
+    "Chico Mall"
+  ];
+  var current;
 
 function shuffleGallery()
 {
+var answer1;
+  do{
+answer1 = Math.floor(Math.random()*Chico.length);
+}
+while(answer1==current);
+current=answer1;
+console.log(Chico[answer1]);
+
 
 }
